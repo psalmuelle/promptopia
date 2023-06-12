863017e25e973a382c71e7a6f1f82c0f160fb3ac
psalmuelle <bjoestores@gmail.com>
2023-06-12 14:12:05 +0100

    feat: Create edit, update and delete prompt

--- app/profile/page.js
+++ app/profile/page.js
@@ -8,6 +8,7 @@ import Profile from '@components/Profile'
 const MyProfile = () => {
     const {data: session} = useSession();
     const [posts, setPosts]=  useState([]);
+    const router = useRouter()
 
     useEffect(()=>{
         const fetchPosts = async()=> {
@@ -20,11 +21,23 @@ const MyProfile = () => {
         if(session?.user.id) fetchPosts()
       }, [])
 
-    const handleEdit=()=>{
-
+    const handleEdit=(post)=>{
+        router.push(`/update-prompt?id=${post._id}`)
     }
-    const handleDelete=()=>{
+    const handleDelete= async(post)=>{
+     const hasConfirmed = confirm('Are you sure you want to delete this prompt?')
+     if(hasConfirmed){
+        try{
+            await fetch(`/api/prompt/${post._id.toString()}`, {
+                method: 'DELETE'
+            } );
+            const filteredPosts = posts.filter((p)=> p._id !== post._id);
 
+            setPosts(filteredPosts);
+        }catch(err){
+            console.log(err)
+        }
+     }
     }
   return (
     <Profile
